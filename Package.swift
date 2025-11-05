1fb864c523bbcdb1aeff6af9d15fec94159ab541
Tirzaan Neil <Tirzaan@tntneil.com>
2025-11-05 11:11:24 -0700

    [Change] - Name

--- Package.swift
+++ Package.swift
@@ -4,19 +4,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "SFSymbol",
+    name: "SFSymbols",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "SFSymbol",
-            targets: ["SFSymbol"]
+            name: "SFSymbols",
+            targets: ["SFSymbols"]
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SFSymbol"
+            name: "SFSymbols"
         ),
 
     ]
